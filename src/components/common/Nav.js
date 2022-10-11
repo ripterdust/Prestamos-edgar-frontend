c743896b8a93d0332cdd9d67c865abc45d2c932d
Bryan Arévalo <barevalo@solcomp.com>
2022-10-11 00:41:34 -0600

    :rocket: Agregada la navbar y la opción de cerrar sesión

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { TokenContext } from '../../hooks/useContextUser'
 
 export const Nav = () => {
+    const { setContext } = useContext(TokenContext)
+
+    const handleAccount = () => {
+        const account = document.querySelector('.account')
+        account.classList.toggle('hide')
+    }
+
+    const logOut = () => {
+        localStorage.removeItem('token')
+        setContext(null)
+    }
     return (
         <nav>
             <div className="strt">
                 <div id="menu">
-                    <i class="fa-solid fa-bars"></i>
+                    <i className="fa-solid fa-bars"></i>
                 </div>
                 <Link to={'/'}>Logo de la empresa</Link>
             </div>
-            <div id="usr">
-                <i class="fa-solid fa-user"></i>
+            <div id="usr" onClick={handleAccount}>
+                <i className="fa-solid fa-user"></i>
+            </div>
+
+            <div className="account hide">
+                <div onClick={logOut}>Cerrar sesión</div>
             </div>
         </nav>
     )
