c6e060829baef51c3b46a13592c8759d0ed688ac
Bryan Arévalo <barevalo@solcomp.com>
2022-10-12 15:03:33 -0600

    :rocket: modificado el footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,12 +3,10 @@ import React from 'react'
 export const Footer = () => {
     return (
         <footer className="main-footer">
-            <strong>
-                Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.
-            </strong>
-            All rights reserved.
+            <strong className="mr-4">Corpodine S.A. &copy;</strong>
+            Todos los derechos reservados
             <div className="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
+                <b>Version</b> 1.0
             </div>
         </footer>
     )
