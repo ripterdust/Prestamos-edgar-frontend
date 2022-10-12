30ed6479868c4d88647f98397b62c4d823e52072
Bryan Arévalo <barevalo@solcomp.com>
2022-10-12 00:12:14 -0600

    :rocket: Eliminado import innecesario

--- src/components/common/base/BrTable.js
+++ src/components/common/base/BrTable.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useContext, useState } from 'react'
-import { Link } from 'react-router-dom'
 import { useTable } from 'react-table'
 import { api } from '../../../api/axios'
 import { mostrarError } from '../../../helpers/mostrarError'
