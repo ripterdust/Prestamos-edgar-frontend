74e82cea2081765031a2d3751cc539ea30ad2583
Bryan Arévalo <barevalo@solcomp.com>
2022-10-12 21:12:17 -0600

    :rocket: Agregadas las opciones de menú

---
+++ src/components/common/general/AdvertenciaCambio.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const AdvertenciaCambio = () => {
+    return (
+        <blockquote className="quote-danger mt-0">
+            <h5 id="reminder">Advertencia</h5>
+            <p>Cualquier cambio en este sitio puede afectar directamente el funcionamiento de la página</p>
+        </blockquote>
+    )
+}
