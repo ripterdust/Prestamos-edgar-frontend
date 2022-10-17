1a78c9dcd26b4a6962406cc3ebfd44401e05260a
lighnio <charlesvas16@outlook.com>
2022-10-17 15:22:57 -0600

    :bulb: Agregado el mantenimiento de monedas

---
+++ src/components/mantenimientos/monedas/Monedas.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useFetch } from '../../../hooks/useFetch'
+import { BrTable } from '../../common/base/BrTable'
+import { AdvertenciaCambio } from '../../common/general/AdvertenciaCambio'
+
+export const Monedas = () => {
+    const [response, setResponse] = useFetch('/monedas')
+    const columns = [
+        {
+            Header: '#',
+            accessor: 'moneda_id',
+        },
+        {
+            Header: 'Nombre',
+            accessor: 'nombre',
+        },
+        {
+            Header: 'Prefijo',
+            accessor: 'Prefix'
+        }
+    ]
+
+    return (
+        <div className="row p-1 w-100">
+            <div className="col-12 p-4">
+                <AdvertenciaCambio />
+                <div className="card">
+                    <h3 className="card-header">Monedas</h3>
+                    <div className="card-body">
+                        <BrTable
+                            columns={columns}
+                            data={response.data}
+                            endpoint="monedas"
+                            identificador="moneda_id"
+                            opcionales={[]}
+                            setFetch={setResponse}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
