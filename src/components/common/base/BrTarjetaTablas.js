4ba779856de4327c5d2c77d9ab312c064c255833
Bryan Arévalo <barevalo@solcomp.com>
2022-10-17 10:54:51 -0600

    :rocket: Agregada un datatable en el dashboard

---
+++ src/components/common/base/BrTarjetaTablas.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const BrTarjetaTablas = ({ titulo = '', columnas = [], rows = [[]] }) => {
+    return (
+        <div class="card w-100">
+            <div className="card-header border-0">
+                <div className="card-title">{titulo}</div>
+            </div>
+
+            <div className="card-body table-responsive p0">
+                <table className="table table-striped table-valign-middle">
+                    <thead>
+                        <tr>
+                            {columnas.map((columna, key) => {
+                                return <th key={key}>{columna}</th>
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows.map((row, index) => {
+                            return (
+                                <tr key={'rows-' + index}>
+                                    {row.map((elemento, key) => {
+                                        return <td key={`row-${key}`}>{elemento}</td>
+                                    })}
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
--- src/components/mantenimientos/Index.js
+++ src/components/mantenimientos/Index.js
@@ -2,15 +2,25 @@ import React from 'react'
 import { BrTarjetaAmarilla } from '../common/base/BrTarjetaAmarilla'
 import { BrTarjetaAzul } from '../common/base/BrTarjetaAzul'
 import { BrTarjetaRoja } from '../common/base/BrTarjetaRoja'
+import { BrTarjetaTablas } from '../common/base/BrTarjetaTablas'
 import { BrTarjetaVerde } from '../common/base/BrTarjetaVerde'
 
 export const Index = () => {
+    const columnasTablaPrestamos = ['Cliente', 'Monto', 'Prestamista', 'Fecha']
+    const filasTablaPrestamos = [
+        ['Bryan Arévalo', 'Q.500.00', 'Pedro Pérez', '15/03/2022'],
+        ['Bryan Arévalo', 'Q.500.00', 'Pedro Pérez', '15/03/2022'],
+        ['Bryan Arévalo', 'Q.500.00', 'Pedro Pérez', '15/03/2022'],
+        ['Bryan Arévalo', 'Q.500.00', 'Pedro Pérez', '15/03/2022'],
+    ]
     return (
         <div className="row p-4">
             <BrTarjetaAzul icono="fa-solid fa-user-tie" texto="Total de usuarios" numero={100} />
-            <BrTarjetaRoja icono="fa-solid fa-user" texto="Total de clientes" numero={100} />
-            <BrTarjetaVerde icono="fa-solid fa-user" texto="Total de clientes" numero={100} />
             <BrTarjetaAmarilla icono="fa-solid fa-user" texto="Total de clientes" numero={100} />
+            <BrTarjetaRoja icono="fa-solid fa-arrow-down" texto="Egresos del mes" numero={100} />
+            <BrTarjetaVerde icono="fa-solid fa-arrow-up" texto="Ingresos del mes" numero={100} />
+
+            <BrTarjetaTablas titulo="Préstamos" columnas={columnasTablaPrestamos} rows={filasTablaPrestamos} />
         </div>
     )
 }
