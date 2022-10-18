411a221c3a586d644ce266af02238c85d3f901ff
ripterdust <bryantello2010@hotmail.com>
2022-10-17 20:02:32 -0600

    :rocket: Eliminado el hg-pages

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { notify } from '../helpers/notify'
 
 export const api = axios.create({
-    baseURL: 'https://137.184.73.135:8000',
+    baseURL: 'http://localhost:8000',
 })
 
 api.interceptors.response.use(undefined, ({ response }) => {
