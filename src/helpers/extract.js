9fb4f4738eb992a8324dbd4cdeb9766e8503aac8
ripterdust <bryantello2010@hotmail.com>
2022-10-18 08:49:16 -0600

    :rocket: Arreglado el bug de el mensaje al iniciar sesión

--- src/helpers/extract.js
+++ src/helpers/extract.js
@@ -1,1 +1,5 @@
-export const extraerTotalRegistros = (data) => (data.data ? data.data[0].totalRegistros : 0)
+export const extraerTotalRegistros = (data) => {
+    const res = data.data ? (data.data[0] ? data.data[0].totalRegistros : 0) : 0
+
+    return res
+}
